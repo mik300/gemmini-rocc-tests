3b3dddbc8db4aa4e3c9d49439a07dbf9717b58bf
Michael <emichael283@gmail.com>
2024-11-19 08:48:44 +0100

    Completed alexnet_params.h

--- imagenet/alexnet_params.h
+++ imagenet/alexnet_params.h
@@ -26,27 +26,44 @@ static const struct ConvParams conv_2_params = {.batch_size=4, .in_row_dim=27, .
  .pool_size=3, .pool_stride=2, .pool_padding=0, .out_dim_pooled=13, .output_scale=(1.0 / (1 << 8))};
 
 
-static const elem_t conv_3_w[2304][256] row_align(1);
-static const acc_t conv_3_b[256] row_align_acc(1);
+static const elem_t conv_3_w[2304][384] row_align(1);
+static const acc_t conv_3_b[384] row_align_acc(1);
 static elem_t conv_3_in row_align(1); 
-static elem_t conv_3_out[4][13][13][256] row_align(1);
+static elem_t conv_3_out[4][13][13][384] row_align(1);
 static const struct ConvParams conv_3_params = {.batch_size=4, .in_row_dim=13, .in_col_dim=13, .kernel_size=3, .in_channels=256,
  .out_channels=384, .stride=1, .padding=1, .bias=1, .depthwise=0, .out_row_dim=13, .out_col_dim=13, .n_patches=676, .patch_size=2304,
  .pool_size=1, .pool_stride=1, .pool_padding=0, .out_dim_pooled=13, .output_scale=(1.0 / (1 << 8))};
 
+// N_PATCHES = (BATCH_SIZE * OUT_ROW_DIM * OUT_COL_DIM);
+// PATCH_SIZE = (KERNEL_DIM * KERNEL_DIM * IN_CHANNELS);
 
+static const elem_t conv_4_w[3456][384] row_align(1);
+static const acc_t conv_4_b[384] row_align_acc(1);
+static elem_t conv_4_in row_align(1); 
+static elem_t conv_4_out[4][13][13][384] row_align(1);
+static const struct ConvParams conv_4_params = {.batch_size=4, .in_row_dim=13, .in_col_dim=13, .kernel_size=3, .in_channels=384,
+ .out_channels=384, .stride=1, .padding=1, .bias=1, .depthwise=0, .out_row_dim=13, .out_col_dim=13, .n_patches=676, .patch_size=3456,
+ .pool_size=1, .pool_stride=1, .pool_padding=0, .out_dim_pooled=13, .output_scale=(1.0 / (1 << 8))};
+
+static const elem_t conv_5_w[3456][256] row_align(1);
+static const acc_t conv_5_b[256] row_align_acc(1);
+static elem_t conv_5_in row_align(1); 
+static elem_t conv_5_out_pooled[4][6][6][256] row_align(1);
+static const struct ConvParams conv_5_params = {.batch_size=4, .in_row_dim=13, .in_col_dim=13, .kernel_size=3, .in_channels=384,
+ .out_channels=256, .stride=1, .padding=1, .bias=1, .depthwise=0, .out_row_dim=13, .out_col_dim=13, .n_patches=144, .patch_size=3456,
+ .pool_size=3, .pool_stride=2, .pool_padding=0, .out_dim_pooled=6, .output_scale=(1.0 / (1 << 8))};
 
 static const elem_t fc_6_w[9216][4096] row_align(1)
-static const acc_t fc_6_b[4][4096] row_align_acc(1)
+static const acc_t fc_6_b[4096] row_align_acc(1)
 static elem_t fc_6_out[4][4096] row_align(1);
 static const struct FcParams fc_6_params = {.batch_size=4, .in_features=9216, .out_features=4096, .bias=1, .output_scale=(1.0 / (1 << 9)), .I=4, .J=4096, .K=9216};
 
 static const elem_t fc_7_w[4096][4096] row_align(1)
-static const acc_t fc_7_b[4][4096] row_align_acc(1)
+static const acc_t fc_7_b[4096] row_align_acc(1)
 static elem_t fc_7_out[4][4096] row_align(1);
 static const struct FcParams fc_7_params = {.batch_size=4, .in_features=4096, .out_features=4096, .bias=1, .output_scale=(1.0 / (1 << 9)), .I=4, .J=4096, .K=4096};
 
 static const elem_t fc_9_w[4096][1000] row_align(1)
-static const acc_t fc_9_b[4][1000] row_align_acc(1)
+static const acc_t fc_9_b[1000] row_align_acc(1)
 static elem_t fc_9_out[4][1000] row_align(1);
 static const struct FcParams fc_9_params = {.batch_size=4, .in_features=4096, .out_features=1000, .bias=1, .output_scale=(1.0 / (1 << 9)), .I=4, .J=1000, .K=9216};
